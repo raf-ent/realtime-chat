25eddb2b5577aa55002b5f34a43b8a1c73b5437b
raf-ent <rufson24@gmail.com>
2025-01-18 06:15:21 -0500

    fix cookies

--- server/utils/jwt.ts
+++ server/utils/jwt.ts
@@ -2,23 +2,18 @@ import { Response } from "express";
 import jwt from "jsonwebtoken";
 
 const genToken = (userID: string, res: Response) => {
-  jwt.sign({ userID }, process.env.TOKEN_SECRET!, { expiresIn: "1d" }, (err, token) => {
-      if (err) {
-        console.log("jwt error", err.message);
-        res.status(500).json({ error: "Internal server error" });
-      } else {
-        res.cookie("token", token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "development",
-            sameSite: "strict",
-            maxAge: 24*3600*1000,
-        });
+  const token = jwt.sign({ userID }, process.env.TOKEN_SECRET!, {
+    expiresIn: "1d"
+  });
 
-        return token;
-      }
-    }
-  );
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "strict",
+    maxAge: 24 * 3600 * 1000,
+  });
+
+  return token;
 };
-
 
 export default genToken;
